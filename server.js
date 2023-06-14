e2b91ebdbc893611267a9af1fb5425ad279951bf
saway11 <rgarcia20.m@gmail.com>
2023-06-14 16:48:05 -0700

    set up a node.js/express app with handlebars as the view engine

--- server.js
+++ server.js
@@ -17,4 +17,14 @@ app.use(session({
         db: sequelize
     })
 }));
-const helpers = require('./utils/helpers')
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/'));
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
