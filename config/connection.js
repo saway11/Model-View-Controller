528e2222565f0b7a04c8fbe9b813a7fab7a19639
saway11 <rgarcia20.m@gmail.com>
2023-06-14 16:57:16 -0700

    sequelize variable is assigned a value

--- config/connection.js
+++ config/connection.js
@@ -3,3 +3,17 @@ const Sequelize = require('sequerlize');
 const dotenv = require('dotenv').config()
 
 let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    console.log(sequelize, 'JAWS_DB true');
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+    console.log(sequelize, 'LOCAL_DB is bad');
+}
+
+module.exports = sequelize;
