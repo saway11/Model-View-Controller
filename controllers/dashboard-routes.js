94f58100fe0be58982f9d95591bcf97235feef3f
saway11 <rgarcia20.m@gmail.com>
2023-06-14 18:16:16 -0700

    added attributes, include, and mode

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -43,4 +43,43 @@ router.get('/', withAuth, (req, res) => {
             loggedIn: true
         });
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', withAuth, (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post',
+            'title',
+            'created_at'
+        ],
+        include: [{
+            model: Comment,
+            attribute: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(400).json({
+                message: 'No post found with this id'
+            });
+            return;
+        }
+    })
 })
