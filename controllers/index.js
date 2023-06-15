09f3464026a50a2393a46323c3083ff05ada506f
saway11 <rgarcia20.m@gmail.com>
2023-06-14 17:04:06 -0700

    configured with additional route modules

--- controllers/index.js
+++ controllers/index.js
@@ -7,3 +7,11 @@ const apiRoutes = require('./api');
 const dashBoard = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
