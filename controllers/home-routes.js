ebdb93bc7f8b840ceadc215fda49c13f6004c5fe
saway11 <rgarcia20.m@gmail.com>
2023-06-14 18:40:28 -0700

    continued to fetch posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,3 +43,51 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/post/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post',
+            'title',
+            'created_at'
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({
+                message: 'No post found with this id'
+            });
+            return;
+        }
+
+        const post = dbPostData.get({
+            plain: true
+        });
+    })
+})
