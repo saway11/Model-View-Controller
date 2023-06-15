ed63559f4f1ea27fb1e8feed4b4db7ed1639929f
saway11 <rgarcia20.m@gmail.com>
2023-06-14 17:38:26 -0700

    use the ID from the session

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -6,3 +6,28 @@ const {
     User,
     Comment
 } = require('../models');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            //use the ID from the session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post',
+            'title',
+            'created_at'
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        
+    ]
+    })
+})
